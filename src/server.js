d345d3558edc0d5f364b4854c5541fd964e5928a
Faisal Javid <faisalljavid@gmail.com>
2025-01-27 14:18:32 +0530

    initialise project

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const NODE_ENV = process.env.NODE_ENV;
+
+const PORT = process.env[`${NODE_ENV}_PORT`];
+
+// const app = express();
+// or
+const server = express();
+
+server.listen(PORT, () => {
+    console.log(`${NODE_ENV} Server is started on PORT - ${PORT}`);
+})
